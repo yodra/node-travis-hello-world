c9d5bf11d7302c6e51aa7abcf74dfe625d4369d6
Yodra <yodralopez@gmail.com>
2018-10-27 20:40:24 +0100

    change test

--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,6 @@
 
-const sum = require('../index');
-
 describe('sum', () => {
     test('should sum two numbers', () => {
-        expect(sum(2, 2)).toBe(4);
+        expect(2 + 2).toBe(4);
     });
 });
