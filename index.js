f82ecaf80cfb901323312461f91df35bb321063e
Yodra <yodralopez@gmail.com>
2018-10-27 16:54:13 +0100

    red test should sum two numbers

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 console.log('Hello world');
+
+function sum(number1, number2) {
+    return number1 + number2;
+}
+
+module.exports = sum;
---
+++ test/index.test.js
@@ -0,0 +1,8 @@
+
+const sum = require('../index');
+
+describe('sum', () => {
+    test('should sum two numbers', () => {
+        expect(sum(2, 2)).toBe(5);
+    });
+});
