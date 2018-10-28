cc193dabd1c18def6857889da7cf4299e97e4c46
Yodra <yodralopez@gmail.com>
2018-10-28 12:26:20 +0000

    remove code js

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-
 const PORT = process.env.PORT || 5000;
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
     res.send('<iframe src="https://giphy.com/embed/Efpfk11XWXwTSKqgV2" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p></p>');
